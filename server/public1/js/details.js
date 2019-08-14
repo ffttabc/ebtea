194ceb2c2b0f9d58fa0914d444dcfb0844866f19
用户名 <>
2019-08-14 11:56:25 +0800

    111111

--- server/public1/js/details.js
+++
@@ -1,9 +0,0 @@
-(function(){
-    ajax({
-        url:"http://localhost:5050/index",
-        type:"get",
-        dataType:"json"
-    }).then(function(result){
-        console.log(result)
-    })
-})()
